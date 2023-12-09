242bb99d66f383ec50d17b1159ff661524def68d
Rain <smalltime153@gmail.com>
2023-12-09 23:56:48 +0800

    first commit

---
+++ src/components/InputDemo.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { debounceRef } from '../utils/debounce'
+const text = debounceRef('')
+console.log(text)
+</script>
+
+<template>
+  <input type="text" v-model="text" />
+  <p class="result">{{ text }}</p>
+</template>
